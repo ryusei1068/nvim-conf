e56f92e13ac3d19e315c020216041861c454a3c0
Ryusei Nohara <nhrryi@gmail.com>
2024-05-18 21:48:02 +0900

    use astro

--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({ "AstroNvim/AstroNvim", version = "^4", import = "astronvim.plugins" })
+
+-- require("config.lazy")
 vim.g.mapleader = " "
